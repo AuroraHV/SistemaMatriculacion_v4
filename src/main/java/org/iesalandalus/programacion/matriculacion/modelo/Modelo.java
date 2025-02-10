91c0b72a857293d5d5b08fe2895884eefcea92ea
Aurora Haro Vicente <aurora.havi@gmail.com>
2025-02-10 16:39:33 +0100

    Modifico los métodos necesarios para utilizar ArrayLists en vez de Arrays en Modelo.

--- src/main/java/org/iesalandalus/programacion/matriculacion/modelo/Modelo.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/modelo/Modelo.java
@@ -1,29 +1,26 @@
 package org.iesalandalus.programacion.matriculacion.modelo;
 
-import org.iesalandalus.programacion.matriculacion.dominio.Alumno;
-import org.iesalandalus.programacion.matriculacion.dominio.Asignatura;
-import org.iesalandalus.programacion.matriculacion.dominio.CicloFormativo;
-import org.iesalandalus.programacion.matriculacion.dominio.Matricula;
+import org.iesalandalus.programacion.matriculacion.dominio.*;
 import org.iesalandalus.programacion.matriculacion.negocio.Alumnos;
 import org.iesalandalus.programacion.matriculacion.negocio.Asignaturas;
 import org.iesalandalus.programacion.matriculacion.negocio.CiclosFormativos;
 import org.iesalandalus.programacion.matriculacion.negocio.Matriculas;
 
 import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
 
 public class Modelo {
 
-    public static final int CAPACIDAD = 10;
     private Alumnos alumnos;
     private Matriculas matriculas;
     private Asignaturas asignaturas;
     private CiclosFormativos ciclosFormativos;
 
     public void comenzar() {
-        this.alumnos = new Alumnos(CAPACIDAD);
-        this.matriculas = new Matriculas(CAPACIDAD);
-        this.asignaturas = new Asignaturas(CAPACIDAD);
-        this.ciclosFormativos = new CiclosFormativos(CAPACIDAD);
+        this.alumnos = new Alumnos();
+        this.matriculas = new Matriculas();
+        this.asignaturas = new Asignaturas();
+        this.ciclosFormativos = new CiclosFormativos();
     }
 
     public void terminar() {
@@ -33,25 +30,25 @@ public class Modelo {
     //Alumno
     public void insertar(Alumno alumno) throws OperationNotSupportedException {
         if (this.alumnos == null) {
-            this.alumnos = new Alumnos(CAPACIDAD);
+            this.alumnos = new Alumnos();
         }
         this.alumnos.insertar(alumno);
     }
     public Alumno buscar(Alumno alumno) {
         if (this.alumnos == null) {
-            this.alumnos = new Alumnos(CAPACIDAD);
+            this.alumnos = new Alumnos();
         }
         return this.alumnos.buscar(alumno);
     }
     public void borrar(Alumno alumno) throws OperationNotSupportedException {
         if (this.alumnos == null) {
-            this.alumnos = new Alumnos(CAPACIDAD);
+            this.alumnos = new Alumnos();
         }
         this.alumnos.borrar(alumno);
     }
-    public Alumno[] getAlumnos() {
+    public ArrayList<Alumno> getAlumnos() {
         if (this.alumnos == null) {
-            this.alumnos = new Alumnos(CAPACIDAD);
+            this.alumnos = new Alumnos();
         }
         return this.alumnos.get();
     }
@@ -59,25 +56,25 @@ public class Modelo {
     //Asignatura
     public void insertar(Asignatura asignatura) throws OperationNotSupportedException {
         if (this.asignaturas == null) {
-            this.asignaturas = new Asignaturas(CAPACIDAD);
+            this.asignaturas = new Asignaturas();
         }
         this.asignaturas.insertar(asignatura);
     }
     public Asignatura buscar(Asignatura asignatura) {
         if (this.asignaturas == null) {
-            this.asignaturas = new Asignaturas(CAPACIDAD);
+            this.asignaturas = new Asignaturas();
         }
         return this.asignaturas.buscar(asignatura);
     }
     public void borrar(Asignatura asignatura) throws OperationNotSupportedException {
         if (this.asignaturas == null) {
-            this.asignaturas = new Asignaturas(CAPACIDAD);
+            this.asignaturas = new Asignaturas();
         }
         this.asignaturas.borrar(asignatura);
     }
-    public Asignatura[] getAsignaturas() {
+    public ArrayList<Asignatura> getAsignaturas() {
         if (this.asignaturas == null) {
-            this.asignaturas = new Asignaturas(CAPACIDAD);
+            this.asignaturas = new Asignaturas();
         }
         return this.asignaturas.get();
     }
@@ -85,25 +82,25 @@ public class Modelo {
     //CicloFormativo
     public void insertar(CicloFormativo cicloFormativo) throws OperationNotSupportedException {
         if (this.ciclosFormativos == null) {
-            this.ciclosFormativos = new CiclosFormativos(CAPACIDAD);
+            this.ciclosFormativos = new CiclosFormativos();
         }
         this.ciclosFormativos.insertar(cicloFormativo);
     }
     public CicloFormativo buscar(CicloFormativo cicloFormativo) {
         if (this.ciclosFormativos == null) {
-            this.ciclosFormativos = new CiclosFormativos(CAPACIDAD);
+            this.ciclosFormativos = new CiclosFormativos();
         }
         return this.ciclosFormativos.buscar(cicloFormativo);
     }
     public void borrar(CicloFormativo cicloFormativo) throws OperationNotSupportedException {
         if (this.ciclosFormativos == null) {
-            this.ciclosFormativos = new CiclosFormativos(CAPACIDAD);
+            this.ciclosFormativos = new CiclosFormativos();
         }
         this.ciclosFormativos.borrar(cicloFormativo);
     }
-    public CicloFormativo[] getCiclosFormativos() {
+    public ArrayList<CicloFormativo> getCiclosFormativos() {
         if (this.ciclosFormativos == null) {
-            this.ciclosFormativos = new CiclosFormativos(CAPACIDAD);
+            this.ciclosFormativos = new CiclosFormativos();
         }
         return this.ciclosFormativos.get();
     }
@@ -111,46 +108,46 @@ public class Modelo {
     //Matrícula
     public void insertar(Matricula matricula) throws OperationNotSupportedException {
         if (this.matriculas == null) {
-            this.matriculas = new Matriculas(CAPACIDAD);
+            this.matriculas = new Matriculas();
         }
         this.matriculas.insertar(matricula);
     }
     public Matricula buscar(Matricula matricula) throws OperationNotSupportedException {
         if (this.matriculas == null) {
-            this.matriculas = new Matriculas(CAPACIDAD);
+            this.matriculas = new Matriculas();
         }
         return this.matriculas.buscar(matricula);
     }
     public void borrar(Matricula matricula) throws OperationNotSupportedException {
         if (this.matriculas == null) {
-            this.matriculas = new Matriculas(CAPACIDAD);
+            this.matriculas = new Matriculas();
         }
         this.matriculas.borrar(matricula);
     }
-    public Matricula[] getMatriculas() throws OperationNotSupportedException {
+    public ArrayList<Matricula> getMatriculas() throws OperationNotSupportedException {
         if (this.matriculas == null) {
-            this.matriculas = new Matriculas(CAPACIDAD);
+            this.matriculas = new Matriculas();
         }
         return this.matriculas.get();
     }
 
-    public Matricula[] getMatriculas(Alumno alumno) throws OperationNotSupportedException {
+    public ArrayList<Matricula> getMatriculas(Alumno alumno) throws OperationNotSupportedException {
         if (this.matriculas == null) {
-            this.matriculas = new Matriculas(CAPACIDAD);
+            this.matriculas = new Matriculas();
         }
         return this.matriculas.get(alumno);
     }
 
-    public Matricula[] getMatriculas(CicloFormativo cicloFormativo) throws OperationNotSupportedException {
+    public ArrayList<Matricula> getMatriculas(CicloFormativo cicloFormativo) throws OperationNotSupportedException {
         if (this.matriculas == null) {
-            this.matriculas = new Matriculas(CAPACIDAD);
+            this.matriculas = new Matriculas();
         }
         return this.matriculas.get(cicloFormativo);
     }
 
-    public Matricula[] getMatriculas(String cursoAcademico) throws OperationNotSupportedException {
+    public ArrayList<Matricula> getMatriculas(String cursoAcademico) throws OperationNotSupportedException {
         if (this.matriculas == null) {
-            this.matriculas = new Matriculas(CAPACIDAD);
+            this.matriculas = new Matriculas();
         }
         return this.matriculas.get(cursoAcademico);
     }
