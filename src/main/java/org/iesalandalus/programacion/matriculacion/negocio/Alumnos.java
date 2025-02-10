dea13406cafe9d412ebc0e4e69f117c4e360026d
Aurora Haro Vicente <aurora.havi@gmail.com>
2025-02-10 16:26:37 +0100

    Modifico los métodos necesarios para utilizar ArrayLists en vez de Arrays en Alumnos.

--- src/main/java/org/iesalandalus/programacion/matriculacion/negocio/Alumnos.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/negocio/Alumnos.java
@@ -2,41 +2,33 @@ package org.iesalandalus.programacion.matriculacion.negocio;
 
 import org.iesalandalus.programacion.matriculacion.dominio.Alumno;
 import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
 
 public class Alumnos {
     //1-Atributos
-    private int capacidad;
-    private int tamano;
-    private Alumno [] coleccionAlumnos;
+    private ArrayList<Alumno> coleccionAlumnos;
 
     //1.1-Constructor con lista de capacidad e inicializa atributos
-    public Alumnos(int capacidad) {
-        if (capacidad <= 0) {
-            throw new IllegalArgumentException("ERROR: La capacidad debe ser mayor que cero.");
-        }
-        this.capacidad = capacidad;
-        this.tamano = 0;
-        this.coleccionAlumnos= new Alumno[capacidad];
+    public Alumnos() {
+        this.coleccionAlumnos= new ArrayList<>();
     }
 
     //Métodos
     //1.2-Get que devuelve copia profunda
-    public Alumno[] get() {
+    public ArrayList<Alumno> get() {
         return copiaProfundaAlumnos();
     }
     //Copia profunda de la colección
-    private Alumno[] copiaProfundaAlumnos() {
-        Alumno[] copiaAlumnos = new Alumno[tamano];
-        for (int i = 0; i < tamano; i++) {
-            copiaAlumnos[i] = new Alumno(coleccionAlumnos[i]);
+    private ArrayList<Alumno> copiaProfundaAlumnos() {
+        ArrayList<Alumno> copiaAlumnos = new ArrayList<>();
+        //Alumno[] copiaAlumnos = new Alumno[this.coleccionAlumnos.size()];
+        for (Alumno a : coleccionAlumnos) {
+            copiaAlumnos.add (new Alumno(a));
         }
         return copiaAlumnos;
     }
     public int getTamano() {
-        return tamano;
-    }
-    public int getCapacidad() {
-        return capacidad;
+        return this.coleccionAlumnos.size();
     }
 
     //1.3-Insertar alumnos al final de la colección mientras no esté repetido o sea nulo
@@ -44,32 +36,12 @@ public class Alumnos {
         if (alumno == null) {
             throw new NullPointerException("ERROR: No se puede insertar un alumno nulo.");
         }
-        if (capacidadSuperada(tamano)) {
-            throw new OperationNotSupportedException("ERROR: No se aceptan más alumnos.");
-        }
-        if (buscarIndice(alumno) != -1) {
+        int indice = this.coleccionAlumnos.indexOf(alumno);
+        if (indice == -1) {
+            this.coleccionAlumnos.add(new Alumno(alumno));
+        } else {
             throw new OperationNotSupportedException("ERROR: Ya existe un alumno con ese dni.");
         }
-        coleccionAlumnos[tamano] = alumno;
-        tamano++;
-    }
-
-    //Buscar el índice de un alumno
-    private int buscarIndice(Alumno alumno) {
-        for (int i = 0; i < tamano; i++) {
-            if (coleccionAlumnos[i].equals(alumno)) {
-                return i;
-            }
-        }
-        return -1; // Si no se encuentra, retornamos -1
-    }
-    //Verificar si el tamaño está superado
-    private boolean tamanoSuperado(int indice) {
-        return indice >= getTamano();
-    }
-    //Verificar si la capacidad está superada
-    private boolean capacidadSuperada(int indice) {
-        return indice >= getCapacidad();
     }
 
     //1.4-Buscar un alumno y devolverlo si está en la colección, en caso contrario será null
@@ -77,11 +49,12 @@ public class Alumnos {
         if (alumno == null) {
             throw new NullPointerException("ERROR: No se puede buscar un alumno nulo.");
         }
-        int indice = buscarIndice(alumno);
-        if (indice != -1) {
-            return new Alumno(get()[indice]); // Devuelve una copia del alumno encontrado
+        int indice = this.coleccionAlumnos.indexOf(alumno);
+        if (indice == -1) {
+            return null;
+        } else {
+            return new Alumno(this.coleccionAlumnos.get(indice));
         }
-        return null;
     }
 
     //1.5-Si un alumno se encuentra en la colección, se borra
@@ -89,20 +62,11 @@ public class Alumnos {
         if (alumno == null) {
             throw new NullPointerException("ERROR: No se puede borrar un alumno nulo.");
         }
-        int indice = buscarIndice(alumno);
-        if (indice != -1) {
-            desplazarUnaPosicionHaciaIzquierda(indice);
-            tamano--;
-        } else {
+        int indice = this.coleccionAlumnos.indexOf(alumno);
+        if (indice == -1) {
             throw new OperationNotSupportedException("ERROR: No existe ningún alumno como el indicado.");
         }
-    }
-    //Tras borrar, se desplazan los elementos hacia la izquierda
-    private void desplazarUnaPosicionHaciaIzquierda(int indice) {
-        for (int i = indice; i < tamano - 1; i++) {
-            coleccionAlumnos[i] = coleccionAlumnos[i + 1];
-        }
-        coleccionAlumnos[tamano - 1] = null;
+        this.coleccionAlumnos.remove(indice);
     }
 }
 
