f294abb64af2d5e92d195a8da5987f4527df3ccf
Aurora Haro Vicente <aurora.havi@gmail.com>
2025-02-10 18:08:20 +0100

    Terminado - v2.

--- src/main/java/org/iesalandalus/programacion/matriculacion/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/MainApp.java
@@ -4,7 +4,6 @@ import org.iesalandalus.programacion.matriculacion.controlador.Controlador;
 import org.iesalandalus.programacion.matriculacion.modelo.Modelo;
 import org.iesalandalus.programacion.matriculacion.vista.Vista;
 
-
 public class MainApp {
 
     public static void main(String[] args) {
