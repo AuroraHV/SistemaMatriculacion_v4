8fa96ec44fcb6e158132778f60b890abc15d3910
Aurora Haro Vicente <aurora.havi@gmail.com>
2025-02-20 04:38:09 +0100

    Creo Modalidad.

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Modalidad.java
@@ -0,0 +1,36 @@
+package org.iesalandalus.programacion.matriculacion.dominio;
+
+public enum Modalidad {
+
+    PRESENCIAL ("Presencial"),
+    SEMIPRESENCIAL ("Semipresencial");
+
+    private String cadenaAMostrar;
+
+    //Constructor
+    private Modalidad(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    //Método imprimir
+    public String imprimir() {
+        int digito = 0;
+        if(cadenaAMostrar == PRESENCIAL.cadenaAMostrar) {
+            digito = 0;
+        } else if(cadenaAMostrar == SEMIPRESENCIAL.cadenaAMostrar) {
+            digito = 1;
+        } else {
+            digito = 2;
+        }
+       return digito + ".-" + cadenaAMostrar;
+    }
+
+    //Representación en texto de los valores
+    @Override
+    public String toString() {
+        return cadenaAMostrar;
+    }
+
+}
+
+
