fb1678eb1b5b2401ce700ec1b6765eb279967ca9
Aurora Haro Vicente <aurora.havi@gmail.com>
2025-02-20 04:39:56 +0100

    Creo grado E.

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/GradoE.java
@@ -0,0 +1,36 @@
+package org.iesalandalus.programacion.matriculacion.dominio;
+
+public class GradoE extends Grado {
+
+    private int numEdiciones;
+
+    public GradoE(String nombre, int numAnios, int numEdiciones) {
+        super(nombre);
+        setNumAnios(numAnios);
+        setNumEdiciones(numEdiciones);
+    }
+
+    public int getNumEdiciones() {
+        return numEdiciones;
+    }
+
+    public void setNumEdiciones(int numEdiciones) {
+        if (numEdiciones < 1) {
+            throw new IllegalArgumentException("ERROR: El número de ediciones un grado E no puede ser menor que 1.");
+        }
+        this.numEdiciones = numEdiciones;
+    }
+
+    @Override
+    public void setNumAnios(int numAnios) {
+        if (numAnios !=1) {
+            throw new IllegalArgumentException("ERROR: El número de años de un grado E debe ser 1.");
+        }
+        this.numAnios = numAnios;
+    }
+
+    @Override
+    public String toString() {
+        return "E: " + super.toString() + " , nº de ediciones: " + numEdiciones;
+    }
+}
