830253fd4b87d9dadc2f62b94c07397578d16246
Aurora Haro Vicente <aurora.havi@gmail.com>
2025-02-20 04:39:46 +0100

    Creo grado D.

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/GradoD.java
@@ -0,0 +1,36 @@
+package org.iesalandalus.programacion.matriculacion.dominio;
+
+public class GradoD extends Grado {
+
+    private Modalidad modalidad;
+
+    public GradoD(String nombre, int numAnios, Modalidad modalidad) {
+        super(nombre);
+        setNumAnios(numAnios);
+        setModalidad(modalidad);
+    }
+
+    public Modalidad getModalidad() {
+        return modalidad;
+    }
+
+    public void setModalidad(Modalidad modalidad) {
+        if (modalidad == null) {
+            throw new NullPointerException("ERROR: La modalidad de un grado D no puede ser nula.");
+        }
+        this.modalidad = modalidad;
+    }
+
+    @Override
+    public void setNumAnios(int numAnios) {
+        if (numAnios < 2 || numAnios > 3) {
+            throw new IllegalArgumentException("ERROR: El número de años de un grado D debe ser 2 o 3.");
+        }
+        this.numAnios = numAnios;
+    }
+
+    @Override
+    public String toString() {
+        return "D: " + super.toString() + " , modalidad: " + modalidad;
+    }
+}
